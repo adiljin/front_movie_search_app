5ca333b5fe22fa130b782bbf7c3a9d2169c843bf
adiljin <adiljins@gmail.com>
2023-02-17 11:33:59 -0500

    Cosmetic changes for the buttons

--- src/hackathon/Navbar.js
+++ src/hackathon/Navbar.js
@@ -11,10 +11,6 @@ export default function Navbar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    {/* <button className="btn btn-outline-light" onClick={() => {
-                        localStorage.clear();
-                        window.location.href = "/login";
-                    }}>Logout</button> */}
                 </div>
             </nav>
         </div>
