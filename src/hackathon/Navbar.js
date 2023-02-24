c51fc9e390c3c740e1f1f320da2a8a0724fb9ba2
adiljin <adiljins@gmail.com>
2023-02-24 11:27:12 -0500

    Fixing navbar link

--- src/hackathon/Navbar.js
+++ src/hackathon/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/pages/Main">React App</a>
+                    <a className="navbar-brand" href="/">React App</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
